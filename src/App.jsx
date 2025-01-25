ef2fe0b73ecf43251e3d4a0fc03320d2b3fb9ebe
Mansi <mansihajare3@gmail.com>
2025-01-25 13:11:37 +0530

    commiting with final navbar and homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import Homepage from "./components/Homepage";
 import ProjectsPage from "./components/projects/ProjectsPage";
 
 function App() {
-
   return (
     <>
-    <div className="w-screen w-min-screen border-1 border-red-600">
- <Homepage />
-      {/* <ProjectsPage/> */}
+      <div className="w-screen w-min-screen border-1 border-red-600">
+        <Homepage />
 
-    </div>
-     
+        {/* to be fixed */}
+        {/* <ProjectsPage/> */}
+      </div>
     </>
   );
 }
