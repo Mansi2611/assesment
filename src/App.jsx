4a1548a1772c575e71019c567a043f8c1339907a
Mansi <mansihajare3@gmail.com>
2025-01-26 21:17:51 +0530

    review page completed just scroll is remaining

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import About from "./components/About/About";
 import Homepage from "./components/Homepage";
 import MarketingITSolutions from "./components/Marketing/MarketingITSolutions";
 import ProjectsPage from "./components/projects/ProjectsPage";
+import Reviews from "./components/Reviews/Reviews";
 import Services from "./components/Services/Services";
 
 function App() {
   return (
     <>
-      <div className="w-screen w-min-screen border-1 border-red-600">
+      <div className="w-screen w-min-screen border-1 border-red-600 overflow-x-hidden">
         <Homepage />
 
         <ProjectsPage/>
@@ -17,6 +18,8 @@ function App() {
 
         <Services/>
         <About/>
+
+        <Reviews/>
       </div>
     </>
   );
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -22,7 +22,9 @@ const About = () => {
           <div className="text-center md:text-left ">
             <p
               className={`text-blue-100 transform transition-all duration-500 ${
-                inView ? "translate-x-0 opacity-100" : "-translate-x-50 opacity-0"
+                inView
+                  ? "translate-x-0 opacity-100"
+                  : "-translate-x-50 opacity-0"
               }`}
             >
               Get Your Free Audit Now!
@@ -30,7 +32,9 @@ const About = () => {
             <h2
               ref={ref}
               className={`text-2xl sm:text-3xl lg:text-3xl text-blue-50 font-bold transform transition-all duration-500 ${
-                inView ? "translate-x-0 opacity-100" : "-translate-x-50 opacity-0"
+                inView
+                  ? "translate-x-0 opacity-100"
+                  : "-translate-x-50 opacity-0"
               }`}
             >
               Are you ready to take the big step towards your digital growth?
@@ -38,9 +42,7 @@ const About = () => {
           </div>
 
           {/* Call-to-Action Button */}
-          <button
-            className="bg-gray-950 border-2 border-gray-950 text-white py-2 px-6 w-full sm:w-auto rounded-full text-sm md:text-base font-medium transition-all duration-300 cursor-pointer flex items-center justify-center"
-          >
+          <button className="bg-gray-950 border-2 border-gray-950 text-white py-2 px-6 w-full sm:w-auto rounded-full text-sm md:text-base font-medium transition-all duration-300 cursor-pointer flex items-center justify-center">
             <span className="flex-grow text-center">Free Growth Plan</span>
             <span className="ml-3 flex justify-center items-center w-8 h-8 bg-white rounded-full">
               <ArrowRightIcon className="w-5 h-5 md:w-6 md:h-6 text-black font-bold" />
@@ -75,51 +77,53 @@ const About = () => {
               and tools to help you discover innovative ways to market, promote,
               and ultimately sell your product or service online.
             </p>
-           <button className="bg-[rgb(213,34,81)] mt-8 border-2 border-[rgb(213,34,81)] text-white py-2 px-6 w-auto sm:w-auto rounded-full text-sm md:text-base font-medium transition-all duration-300 cursor-pointer flex items-center">
-  <span className="flex-grow text-center">Read More</span>
-  <span className="ml-3 flex justify-center items-center w-8 h-8 bg-white rounded-full">
-    <ArrowRightIcon className="w-6 h-5 md:w-6 md:h-6 text-black font-bold" />
-  </span>
-</button>
-
+            <button className="bg-[rgb(213,34,81)] mt-8 border-2 border-[rgb(213,34,81)] text-white py-2 px-6 w-auto sm:w-auto rounded-full text-sm md:text-base font-medium transition-all duration-300 cursor-pointer flex items-center">
+              <span className="flex-grow text-center">Read More</span>
+              <span className="ml-3 flex justify-center items-center w-8 h-8 bg-white rounded-full">
+                <ArrowRightIcon className="w-6 h-5 md:w-6 md:h-6 text-black font-bold" />
+              </span>
+            </button>
           </div>
 
           {/* Right Side */}
           <div className="lg:w-1/2 sm:w-[80%] w-[90%] mx-auto px-4 flex items-center justify-center bg-white relative">
-  <div className="w-full h-full relative">
-    {/* Word Boxes */}
-    <div className="absolute left-[-13%] top-1/2 transform -translate-y-1/2 flex flex-col gap-3 ">
-      {[
-        "Safe & Secure",
-        "Online Support",
-        "Finish on Time",
-        "Friendly TOS",
-        "24/7 Days Support",
-      ].map((word, index) => (
-        <div
-          key={index}
-          className={`py-3 px-4 bg-white/50 border border-gray-300 flex items-center justify-center rounded-lg text-sm font-medium text-gray-700 transform transition-all duration-1000 ${
-            inView ? "translate-x-0 opacity-100" : "-translate-x-12 opacity-0"
-          }`}
-          style={{ transitionDelay: `${index * 200}ms` }}
-        >
-          {word}
-        </div>
-      ))}
-    </div>
+            <div className="w-full h-full relative">
+              {/* Word Boxes */}
+              <div className="absolute left-[-13%] top-1/2 transform -translate-y-1/2 flex flex-col gap-3 ">
+                {[
+                  "Safe & Secure",
+                  "Online Support",
+                  "Finish on Time",
+                  "Friendly TOS",
+                  "24/7 Days Support",
+                ].map((word, index) => (
+                  <div
+                    key={index}
+                    className={`py-3 px-4 bg-white/50 border border-gray-300 flex items-center justify-center rounded-lg text-sm font-medium text-gray-700 transform transition-all duration-1000 ${
+                      inView
+                        ? "translate-x-0 opacity-100"
+                        : "-translate-x-12 opacity-0"
+                    }`}
+                    style={{ transitionDelay: `${index * 200}ms` }}
+                  >
+                    {word}
+                  </div>
+                ))}
+              </div>
 
-    {/* Image */}
-    <img
-      src="/assets/images/about.webp"
-      alt="About Matrix Bricks"
-      className="w-full object-cover rounded-tl-[20%] rounded-br-[20%]"
-    />
-  </div>
-</div>
-
-
+              {/* Image */}
+              <img
+                src="/assets/images/about.webp"
+                alt="About Matrix Bricks"
+                className="w-full object-cover rounded-tl-[20%] rounded-br-[20%]"
+              />
+            </div>
+          </div>
         </div>
       </div>
+
+
+      {/* technologies and partners components t be done  */}
     </div>
   );
 };
